4187008da2c9f37b91c62b84b758e7020a0a7783
Ryan Bender <ryantb813@gmail.com>
2024-09-10 22:12:12 -0500

    Add to new branch

---
+++ program2.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello, welcome to Program 2 of my Git learning journey! I have exhausted all my resources creating Program 1, so I apologize for the disappointment.")
+    print("Thank you for stopping by to this file! I hope you have a great rest of your day!")
+
+    
+if __name__=="__main__":
+    main()
